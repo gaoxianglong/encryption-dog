301193798b3bb431f3ad56a3f052f73bddcce094
gao_xianglong <gao_xianglong@sina.com>
2022-05-16 23:08:11 +0800

    -o, --only-local命令增强

---
+++ src/main/java/com/github/utils/IdWorker.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2019-2119 gao_xianglong@sina.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.utils;
+
+/**
+ * 
+ * @author gao_xianglong@sina.com
+ * @version 0.1-SNAPSHOT
+ * @date created in 2022/5/16 18:18
+ */
+public interface SequnceID<T> {
+    /**
+     * 获取全局唯一ID
+     *
+     * @return
+     */
+    T getSequnceID();
+}
